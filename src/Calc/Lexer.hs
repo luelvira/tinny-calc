5b8f0e8ab749b051205d716bdce7dd6861c047ac
Lucas Elvira Martín <lucaselvira96@gmail.com>
2024-07-08 16:57:18 +0200

    Generate a list of Tokens from the input
    This module has less competences rather the  but it follows better the Desing of a parser

---
+++ src/Calc/Lexer.hs
@@ -0,0 +1,88 @@
+module Calc.Lexer where
+
+import Data.Char
+
+import qualified Calc.Equation as Eq
+
+data Token
+  = TokLeftParen
+  | TokRightParen
+  | TokEquals
+  | TokDot
+  | TokOperator Eq.Operation
+  | TokNumber !Double
+  | TokEof
+  | TokEos -- Semi collon end of sentence
+  | TokError
+  deriving (Show, Eq)
+
+data LexState = LexState
+  { lexPos :: Peek
+  , lexSource :: String
+  , lexContext :: [Token]
+  }
+  deriving (Show, Eq)
+
+newtype Peek = Peek (Int, Int) deriving (Show, Eq)
+
+move :: Peek -> Peek -> Peek
+(move) (Peek (l1, c1)) (Peek (l2, c2)) = Peek (l1 + l2, c1 + c2)
+  
+
+-- | Convert the streem into tokens
+lexicalParser :: String -> [Token]
+lexicalParser src = lex' LexState {
+  lexPos = Peek (1, 1),
+  lexSource = src,
+  lexContext = []
+  } src
+
+lex' :: LexState -> String -> [Token]
+lex' state "" = lexContext state <> [TokEof]
+lex' state src =
+  let
+    (token, readded) = nextWhileNotWhite "" 0 src
+    tokenType = convertToToken token
+    _lexContext = lexContext state
+    newState = LexState {
+      lexContext = _lexContext <> [tokenType],
+      lexSource = lexSource state,
+      lexPos = lexPos state `move` Peek (0, readded)
+      }
+  in
+    lex' newState (updateSource src readded)
+
+
+nextWhileNotWhite ::
+  String ->
+  Int ->
+  String ->
+  (String, Int)
+nextWhileNotWhite "" cursor (' ':xs) = nextWhileNotWhite "" (cursor + 1) xs
+nextWhileNotWhite context cursor (' ':_) = (context, cursor)
+nextWhileNotWhite context cursor (c:rest) = nextWhileNotWhite (context <> [c]) (cursor + 1) rest
+nextWhileNotWhite context cursor "" = (context, cursor)
+
+convertToToken :: String -> Token
+convertToToken text
+  | text == "(" = TokLeftParen
+  | text == ")" = TokRightParen
+  | text == "+" = TokOperator Eq.Sumatory
+  | text == "-" = TokOperator Eq.Difference
+  | text == "/" = TokOperator Eq.Division
+  | text == "*" = TokOperator Eq.Multiplication
+  | text == ";" = TokEos
+  | all isDigit text = TokNumber (read text :: Double)
+  | otherwise = TokError
+  
+copyLexState :: LexState -> LexState
+copyLexState source = LexState
+                    { lexPos    = lexPos source
+                    , lexSource = lexSource source
+                    , lexContext = lexContext source
+                    }
+
+updateSource :: String -> Int -> String
+updateSource (x:xs) 0 = x:xs
+updateSource "" _ = ""
+updateSource (_:xs) letters = updateSource xs (letters - 1)
